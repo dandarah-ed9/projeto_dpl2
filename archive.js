3170625a9b904193bcee50a60c27f4fb0a3a6770
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-05-26 10:47:11 -0300

    joguinho

--- archive.js
+++ archive.js
@@ -9,24 +9,45 @@ var perna2 = canvas.getContext('2d');
 var perna3 = canvas.getContext('2d');
 var perna4 = canvas.getContext('2d');
 var piso  = canvas.getContext('2d');
+var estojo = canvas.getContext('2d');
 var lapis = canvas.getContext('2d');
 var borracha = canvas.getContext('2d');
 var regua = canvas.getContext('2d');
 
+var x = {};
+document.addEventListener("keydown", (event)=>{
+    x[event.key] = true;
+});
+document.addEventListener("keyup", (event)=> {
+    x[event.key] = false;
+});
+
+function animate(){
+    requestAnimationFrame(animate);
+
+    estojo.fillStyle = 'green'
+    if (x["ArrowRight"]) x + 10; 
+    if (x["ArrowLeft"])  x - 10; 
+    estojo.fillRect(x, 430, 100, 200);
+}
+
 mesa1.fillStyle = 'brown';
-mesa1.fillRect(40, 70, 700, 50);
+mesa1.fillRect(0, 70, 700, 50);
 
 mesa2.fillStyle = 'brown';
 mesa2.fillRect(820, 70, 700, 50);
 
-perna1.fillStyle = 'gray';
-perna1.fillRect(40, 120, 80, 400);
+perna1.fillStyle = 'black';
+perna1.fillRect(0, 120, 80, 400);
 
-perna2.fillStyle = 'gray';
-perna2.fillRect(660, 120, 80, 400);
+perna2.fillStyle = 'black';
+perna2.fillRect(620, 120, 80, 400);
 
-perna3.fillStyle = 'gray';
+perna3.fillStyle = 'black';
 perna3.fillRect(820, 120, 80, 400);
 
-perna4.fillStyle = 'gray';
+perna4.fillStyle = 'black';
 perna4.fillRect(1440, 120, 80, 400);
+
+piso.fillStyle = 'gray'
+piso.fillRect(0, 430, 3000, 400);
